a27aca20dd70c4173b1750e528bbafd84b567d5e
Luca Parise <lucaparise100@gmail.com>
2024-05-13 16:04:43 +0200

    dqwe

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -1,11 +1,108 @@
 package it.unipd.mtss;
 
+import java.util.Arrays;
+
 public class RomanPrinter {
-    public static String print(int num){
-        return printAsciiArt(IntegerToRoman.convert(num));
+
+  public static String print(int num) /*throws ZeroException, NegativeNumberException,
+   BiggerThan4000Exception, NotRomanLetterException*/ {
+  return printAsciiArt(IntegerToRoman.convert(num));
+  }
+
+  private static String printAsciiArt(String romanNumber) /*throws NotRomanLetterException*/{
+
+
+    String[] I ={
+                "  _____  ",
+                " |_   _| ",
+                "   | |   ", 
+                "   | |   ", 
+                "  _| |_  ", 
+                " |_____| "};
+
+
+    String[] V ={
+                " __      __ ", 
+                " \\ \\    / / ",
+                "  \\ \\  / /  ", 
+                "   \\ \\/ /   ", 
+                "    \\  /    ", 
+                "     \\/     "};
+
+
+    String[] X ={" __   __  ", 
+                 " \\ \\ / /  ",
+                 "  \\ V /   ",
+                 "   > <    ",
+                 "  / . \\   ",
+                 " /_/ \\_\\  "
+                };
+
+
+    String[] L ={
+                "  _       ", 
+                " | |      ",
+                " | |      ",
+                " | |      ",
+                " | |____  ",
+                " |______| "
+            };
+
+
+    String[] C ={
+                "   _____   ",  
+                "  / ____|  ",
+                " | |       ",
+                " | |       ",
+                " | |____   ",
+                "  \\_____|  " 
+            };
+
+
+    String[] D ={
+                "  ____    ",
+                " |  __ \\  ",
+                " | |  | | ",
+                " | |  | | ",
+                " | |__| | ",
+                " |_____/  " 
+            };
+
+
+
+    String[] M ={
+                "  __  __   ",
+                " |  \\/  |  ",
+                " | \\  / |  ",
+                " | |\\/| |  ",
+                " | |  | |  ",
+                " |_|  |_|  "};
+
+
+    String result="";
+
+
+    for (int j=0; j<6;j++){
+        
+        for (char c : romanNumber.toCharArray()) {
+            if(c =='I') {result+= I[j];}
+            if(c =='V') {result+= V[j];}
+            if(c =='X') {result+= X[j];}
+            if(c =='L') {result+= L[j];}
+            if(c =='C') {result+= C[j];}
+            if(c =='D') {result+= D[j];}
+            if(c =='M') {result+= M[j];}
+            
+            
+            if (!(Arrays.asList('I', 'V', 'X', 'L', 'C', 'D', 'M').contains(c))){
+                //throw new NotRomanLetterException(c);
+            }
+        }
+
+        result+="\n";
     }
-    private static String printAsciiArt(String romanNumber){
-        //TODO
-        return null;
-    }
-}
+
+    return result;
+  }
+   
+};
